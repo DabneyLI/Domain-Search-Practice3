e17034b3d2fa0384b601ef99ada3c168f31bf6af
DabneyLI <144627567+DabneyLI@users.noreply.github.com>
2023-12-17 21:32:04 +0100

    update

--- my-app-next-js/pages/index.js
+++ my-app-next-js/pages/index.js
@@ -1,12 +1,17 @@
 // pages/index.js
+import React from 'react';
+
 export default function Home() {
+  // 注意：handleSubmit 不再需要，因为我们直接通过表单进行跳转
+
   return (
     <div>
       <h1>WHOIS Query</h1>
-      <form method="GET" action="/results">
+      <form method="GET" action="/results" target="_blank">
         <input name="query" type="search" placeholder="Enter domain (e.g., example.com)" required />
         <button type="submit">Search</button>
       </form>
     </div>
   );
 }
+
